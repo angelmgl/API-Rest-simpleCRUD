d4d76b98c96a38e4ed19d12eef957b87c034ebdf
Angel Mgl <angelemegeele@gmail.com>
2020-12-04 21:02:48 -0300

    Comprobación de datos ingresados a la API

--- controllers/products.js
+++ controllers/products.js
@@ -1,48 +1,88 @@
 const { products } = require('../db.json');
 
+const regex = /^[a-z]{2,20}$/;
+
 module.exports = {
     getProducts: (req, res) => {
         res.json({ products: products});
     },
 
     addProduct: (req, res) => {
-        const { name } = req.body;
-        products.push({
-            id: products.length,
-            name
-        });
-        res.json({
-            "success": true,
-            "message": "Successfully added"
-        });
+        let { name } = req.body;
+        name = name.trim().toLowerCase();
+
+        if(regex.test(name)) {
+            products.push({
+                id: products.length,
+                name
+            });
+        
+            res.status(201).json({
+                "success": true,
+                "message": "Successfully added"
+            });
+        } else {
+            res.status(400).json({
+                "success": false,
+                "message": "Name must be string, don't include numbers or spaces."
+            });
+        }
     },
 
     updateProduct: (req, res) => {
         const { id } = req.params;
-        const { name } = req.body;
+        let { name } = req.body;
+        name = name.trim().toLowerCase();
+        let exist = false;
 
-        products.forEach((product, i) => {
-            if(product.id === Number(id)) {
-                product.name = name;
+        if(regex.test(name)) {
+            products.forEach((product, i) => {
+                if(product.id === Number(id)) {
+                    id.name = name;
+                    exist = true;
+                }
+            });
+
+            if(!exist) {
+                res.status(400).json({
+                    "success": false,
+                    "message": "We don't have any object with that id, check it and try again."
+                });
+                return;
             }
-        });
-
-        res.json({
-            "success": true,
-            "message": "Successfully updated"
-        });
+    
+            res.status(200).json({
+                "success": true,
+                "message": "Successfully updated"
+            });
+        } else {
+            res.status(400).json({
+                "success": false,
+                "message": "Name must be string, don't include numbers or spaces."
+            });
+        }
     },
 
     deleteProduct: (req, res) => {
         const { id } = req.params;
+        let exist = false;
 
         products.forEach((product, i) => {
             if(product.id === Number(id)) {
                 products.splice(i, 1);
+                exist = true;
             }
         });
 
-        res.json({
+        if(!exist) {
+            res.status(400).json({
+                "success": false,
+                "message": "We don't have any object with that id, check it and try again."
+            });
+            return;
+        }
+
+        res.status(200).json({
             "success": true,
             "message": "Successfully deleted"
         });
